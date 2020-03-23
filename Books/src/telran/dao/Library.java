d9d880fe167006b675df19735418d7f5f77beed2
Ovsiannikov78 <seri_tanya@mail.ru>
2020-03-23 13:10:41 +0100

    Task Library added a method bookSearch HW

---
+++ Books/src/telran/dao/Library.java
@@ -0,0 +1,71 @@
+package telran.dao;
+
+import telran.data.Book;
+
+public class Library {
+    private Book[] books;
+    private int size;
+
+    public Library(int capacity) {
+        books = new Book[capacity];
+        size = 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public boolean addBook(Book book) {
+        if (size < books.length) {
+            books[size] = book;
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean deleteBook(Book book) {
+        System.out.println("current size Library is " + size);
+        for (int i = 0; i < size; i++) {
+            if (books[i].equals(book)) {
+                books[i] = books[size - 1];
+                size--;
+                System.out.println("size Library is " + size);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void display() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(books[i]);
+        }
+    }
+
+    public void searchBookByAuthor(String author) {
+        boolean flag = false;
+        for (int i = 0; i < size; i++) {
+            if (books[i].getAuthor().equalsIgnoreCase(author)) {
+                System.out.println(books[i]);
+                flag = true;
+            }
+        }
+        if (!flag) {
+            System.out.println("sorry, not found");
+        }
+    }
+               /* ДОМАШКА  */
+    public void findBook(Book book) {
+        boolean flag = false;
+        for (int i = 0; i < size ; i++) {
+            if (books[i].equals(book)) {
+                System.out.println("Your book is in the cell under the number: " + i);
+                flag = true;
+            }
+        }
+        if(!flag)
+            System.out.println("We don't have this book in our library");
+    }
+
+}
